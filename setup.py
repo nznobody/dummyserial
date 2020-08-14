9c77e5efc87bf560cd923656559611b7701be081
Martijn <martijn@cobaltum.nl>
2020-08-14 16:11:18 +0200

    fixing PyPi setup

--- setup.py
+++ setup.py
@@ -1,49 +1,23 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-Setup for the Dummy Serial Python Module.
-
-Source:: https://github.com/ampledata/dummyserial
-"""
-
-
-__title__ = 'dummyserial'
-__version__ = '1.1.0'
-__author__ = 'Greg Albrecht <gba@orionlabs.io>'
-__license__ = 'Apache License, Version 2.0'
-__copyright__ = 'Copyright 2016 Orion Labs, Inc.'
-
-
-import os
 import setuptools
-import sys
 
-
-def publish():
-    """Function for publishing package to pypi."""
-    if sys.argv[-1] == 'publish':
-        os.system('python setup.py sdist upload')
-        sys.exit()
-
-
-publish()
-
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
-    name='dummyserial',
-    version=__version__,
-    description='Dummy Serial Implementation.',
-    author='Greg Albrecht',
-    author_email='gba@orionlabs.io',
-    packages=['dummyserial'],
-    package_data={'': ['LICENSE']},
-    license=open('LICENSE').read(),
-    long_description=open('README.rst').read(),
-    url='https://github.com/ampledata/dummyserial',
-    setup_requires=['coverage >= 3.7.1', 'nose >= 1.3.7'],
-    install_requires=['pyserial >= 2.7'],
-    package_dir={'dummyserial': 'dummyserial'},
-    zip_safe=False,
-    include_package_data=True
+    name="dummyserial", # Replace with your own username
+    version="1.1.0",
+    author="Greg Albrecht",
+    copyright='Copyright 2016 Orion Labs, Inc.',
+    author_email="gba@orionlabs.io",
+    description="Dummy Serial Implementation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ampledata/dummyserial",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
