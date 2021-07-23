b47b8792d0b783b59f9b4618a9a1262a2cac71f7
Manu Lange <Manu.Lange@plantandfood.co.nz>
2021-07-23 15:00:56 +1200

    fix type

--- dummyserial/classes.py
+++ dummyserial/classes.py
@@ -8,7 +8,7 @@ import logging.handlers
 import sys
 import time
 
-from serial.serialutil import SerialException, portNotOpenError
+from serial.serialutil import SerialException, PortNotOpenError
 
 import dummyserial.constants
 
@@ -115,7 +115,7 @@ class Serial(object):
             self._logger.debug('Writing bytes(%s): %s', len(data), self._ashex(data))
 
         if not self._isOpen:
-            raise portNotOpenError
+            raise PortNotOpenError
 
         self._waiting_data = self._check_response(data)
         return
@@ -139,7 +139,7 @@ class Serial(object):
         self._logger.debug('Reading %s bytes.', size)
 
         if not self._isOpen:
-            raise portNotOpenError
+            raise PortNotOpenError
 
         if size < 0:
             raise dummyserial.exceptions.DSIOError(
